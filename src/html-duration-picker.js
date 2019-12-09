960a337007e826ce9e572183ad38ba6d14ca44bb
Dan Chif <nadchif@gmail.com>
2019-12-09 17:36:47 +0200

    removed console.log()
    Removed  console.log(event.target.dataset.duration);

--- src/html-duration-picker.js
+++ src/html-duration-picker.js
@@ -122,7 +122,6 @@ export default (function() {
 
   // validate any input in the box;
   const validateInput = (event) => {
-    console.log(event.target.dataset.duration);
     const sectioned = event.target.value.split(':');
     if (sectioned.length !== 3) {
       event.target.value = event.target.dataset.duration || '00:00:00'; // fallback to default
