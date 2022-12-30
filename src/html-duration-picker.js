2e8ff9b97897be6ba2a6a6c04b28221b41d0115c
dan <>
2022-12-30 15:13:26 +0800

    REFACTOR: angular fixes

--- src/html-duration-picker.js
+++ src/html-duration-picker.js
@@ -11,7 +11,7 @@
  *
  */
 
-export default (function () {
+ export default (function () {
   // IE9+ support forEach:
   if (window.NodeList && !window.NodeList.prototype.forEach) {
     NodeList.prototype.forEach = Array.prototype.forEach;
@@ -138,13 +138,13 @@ export default (function () {
    * @return {Event}
    */
   const createEvent = (type, option = {bubbles: false, cancelable: false}) => {
-    if (typeof Event === 'function') {
-      return new Event(type);
-    } else {
+    // if (typeof Event === 'function') {
+    //   return new Event(type, option);
+    // } else {
       const event = document.createEvent('Event');
       event.initEvent(type, option.bubbles, option.cancelable);
       return event;
-    }
+    // }
   };
 
   /**
@@ -152,14 +152,16 @@ export default (function () {
    * @param {*} inputBox
    * @param {Number} secondsValue value in seconds
    * @param {Boolean} dispatchSyntheticEvents whether to manually fire 'input' and 'change' event for other event listeners to get it
+   * @param {Number} adjustmentFactor the adjustment factor in seconds
    */
-  const insertFormatted = (inputBox, secondsValue, dispatchSyntheticEvents) => {
+  const insertFormatted = (inputBox, secondsValue, dispatchSyntheticEvents, adjustmentFactor = 1) => {
     const hideSeconds = shouldHideSeconds(inputBox);
     const formattedValue = secondsToDuration(secondsValue, hideSeconds);
     const existingValue = inputBox.value;
     // Don't use setValue method here because
     // it breaks the arrow keys and arrow buttons control over the input
     inputBox.value = formattedValue;
+    // save current cursor location for automatic increase
 
     // manually trigger an "input" event for other event listeners
     if (dispatchSyntheticEvents !== false) {
@@ -168,6 +170,9 @@ export default (function () {
       }
       inputBox.dispatchEvent(createEvent('input'));
     }
+    inputBox.setAttribute('data-adjustment-factor', adjustmentFactor);
+    console.log({adjustmentFactor});
+    highlightTimeUnitArea(inputBox, adjustmentFactor);
   };
 
   /**
@@ -242,7 +247,7 @@ export default (function () {
         break;
     }
     const constrainedValue = applyMinMaxConstraints(inputBox, secondsValue);
-    insertFormatted(inputBox, constrainedValue, true);
+    insertFormatted(inputBox, constrainedValue, true, adjustmentFactor);
   };
 
   /**
@@ -752,15 +757,12 @@ export default (function () {
           btn.addEventListener('mousedown', (event) => {
             event.target.style.transform = 'translateY(1px)';
             event.preventDefault();
-            const adjustmentFactor = getAdjustmentFactor(inputBox);
             if (btn == scrollUpBtn) {
               changeValueByArrowKeys(inputBox, 'up');
-              highlightTimeUnitArea(inputBox, adjustmentFactor);
-              // intervalId = setInterval(changeValueByArrowKeys, 200, inputBox, 'up');
+              intervalId = setInterval(changeValueByArrowKeys, 200, inputBox, 'up');
             } else {
               changeValueByArrowKeys(inputBox, 'down');
-              highlightTimeUnitArea(inputBox, adjustmentFactor);
-              // intervalId = setInterval(changeValueByArrowKeys, 200, inputBox, 'down');
+              intervalId = setInterval(changeValueByArrowKeys, 200, inputBox, 'down');
             }
           });
           // handle enter key to increase value, for better accessibility ux
